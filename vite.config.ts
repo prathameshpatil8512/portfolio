39d9dd2f4b51dd185ca321f07c39907015eb0216
prathameshpatil8512 <prathameshpatil8512@yahoo.com>
2025-10-07 11:19:12 +0530

    fifth commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // 👈 your repository name
-});
+  base: '/portfolio/', // 👈 your GitHub repo name
+})
